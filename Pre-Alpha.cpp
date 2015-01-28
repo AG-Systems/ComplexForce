2662bd942ad4607040a3914fa2b4cb16587bbc56
MeepoKid <Empathy@smarttechadvice.com>
2015-01-27 23:56:50 -0800

    Pre-Alpha
    Practicing and typing out y/n questions.

---
+++ Pre-Alpha.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int y, n;
+	int result;
+	std::cout << "Do you want to see meepo dance?" << endl;
+	cin >> result;
+	std::cout << result << std::endl;
+	//cin >> type;
+	if (result == 'y')
+	std::cout << "SANDSTORM ＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤ" << endl;
+
+	else if (result == 'n');
+	std::cout << "D'awwwwww whatever then" << endl;
+	
+std::cout << result << std::endl;
+ cout << result;
+ system("pause");
+ return 0;
+ 
+}
+//while (!cin.fail() && type != 'y' && type != 'n');
