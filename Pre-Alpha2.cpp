fd85fb8ee4c36586b251c0734cc9b7957755ef3d
MeepoKid <area51alienware@gmail.com>
2015-01-29 00:47:26 -0800

    Pre-Alpha2.cpp
    Does not work currently

---
+++ Pre-Alpha2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int y, n;
+	int result;
+	std::cout << "Do you want to see meepo dance?" << endl;
+	
+	cin >> "y";
+	if (result == 'y' );
+	std::cout << "boom" << endl;
+
+	if (cin >> "n");
+	if (result == 'n' );
+	std::cout << "D'awwwwww whatever then" << endl;
+
+	
+
+}
+//while (!cin.fail() && type != 'y' && type != 'n');
+//SANDSTORM ＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤＵＤ//
+//std::cout << result << std::endl;
+//printf("yessss!!!!");
