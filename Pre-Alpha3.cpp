7ea27a214628dc00d66987a9452f65eee7a08eca
MeepoKid <area51alienware@gmail.com>
2015-01-31 19:24:21 -0800

    Pre-Alpha3.cpp

---
+++ Pre-Alpha3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char result;
+
+	std::cout << "Do you want to see meepo dance?" << endl;
+	cin >> result;
+	std::cout << result << std::endl;
+	//cin >> type;
+	if (result == 'y')
+		std::cout << "SANDSTORM " << endl;
+
+	else if (result == 'n')
+
+		std::cout << "D'awwwwww whatever then" << endl;
+
+	std::cout << result << std::endl;
+	cout << result;
+	system("pause");
+	return 0;
+
+}
+//while (!cin.fail() && type != 'y' && type != 'n');
