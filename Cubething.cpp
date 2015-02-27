2c3171d92cb767fd0ec71d06776218746596a45c
MeepoKid <area51alienware@gmail.com>
2015-02-26 16:50:31 -0800

    Create Cubething.cpp

---
+++ Cubething.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main() {
+std::cout << What method do you want to use to brute force? << std::endl;
+
+}
+
+enum Methods {
+NUMBERS,
+ALPHA,
+ALPHANUMBERIC,
+}
+  switch (if) {
+case 1:
+    1 = NUMBERS
+  break;
+
+case 2:
+    2 = ALPHA
+  break;
+
+case 3:
+    3 = APLHANUBERIC
+  break;
+}
+
+switch (Methods) {
+case NUMBERS:
+    std::cout << "123456789 password cracked" << endl;
+  break;
+case ALPHA:
+    std::cout << "meepo is hot passsword cracked" << endl;
+  break,
+case ALPHANUBERIC:
+    std::cout << "meepo is hot4 password cracked" <<endl;
+  break;
+}
