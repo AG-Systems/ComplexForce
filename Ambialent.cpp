a51bc2ca3e15a884630d6baf7bcdc6e5276838da
MeepoKid <area51alienware@gmail.com>
2015-03-17 16:49:25 -0700

    Ambialent.cpp
    asdfasdfsadf

---
+++ Ambialent.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+std::string ambivalent = "ambivalent";
+
+int main()	{
+
+for (int i = 0; i < ambivalent.length(); ++i)	{
+	std::cout<< ambivalent[i] <<std::endl;
+	}
+
+}
+
+for (int i = 0; i < ambivalent.length(); ++i)
+{
+	
+}
+
